b71a739bec51d5a4e62e18d89b85f460c949a622
Joseph Salisbury <33296248+jpsalis@users.noreply.github.com>
2022-08-08 17:27:20 -0400

    appeasing pylint

--- project.py
+++ project.py
@@ -4,34 +4,40 @@ import yaml # Might use for config?
 from pathlib import Path # Might use for file operations?
 
 class Morse:
-    def __init__(self, database:str = 'morse.yaml'):
+    def __init__(self, db:str = 'morse.yaml'):
+        '''Initialize Morse class. 
+        Takes in a reference to a .yaml file or dictionary of single character keys.'''
         # Import YAML file with morse data
-        with open(database) as file:
+        with open(db) as file:
             self.lookup = yaml.load(file, Loader=yaml.BaseLoader)
-            if not all(len(c) == 1 and isinstance(c) == str for c in self.lookup):
+            if not all(len(ch) == 1 and isinstance(ch) == str for ch in self.lookup):
                 raise ValueError('All keys in .yaml file must be a char.')
 
-        # If exists, import profile defaults from the correct directory.    
+        # If exists, import profile defaults from the correct directory.
         self.dot = '.'
         self.dash= '-'
 
 
 
 
-        # default_file will be a 
-    def txt_to_morse(self, txt: str):
+    
+    def txt_to_morse(self, txt: str) -> str:
+        '''Given a string of text, use lookup array to generate morse code equivalent of text.'''
         to_return = ''
         for c in txt:
             if c in self.lookup:
-                to_return += self.lookup[c]
+                to_return += self.lookup[c] 
 
         return to_return
 
-    def morse_to_txt(self, morse: str):
+    def morse_to_txt(self, morse: str) -> str:
+        '''Given a string of morse, use lookup array to generate text equivalent.'''
         ...
 
     @classmethod
     def touch_config():
+        '''Creates configuration file and prints its location on disk.
+        May remove.'''
         # Create config file if it doesn't exist
         ...
 
