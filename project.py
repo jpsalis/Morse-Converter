57b295427a25f1dfc4a248b6db566b1c9b0a8046
Joseph Salisbury <jpsalis@pop-os.localdomain>
2022-08-08 17:49:29 -0400

    pylint initial style correction.

--- project.py
+++ project.py
@@ -1,14 +1,19 @@
+'''Morse Code converter.
+Given a text file or string with text or morse,
+the user is returned a string of the opposite type.'''
 #import sys
 #import argparse
 import yaml # Might use for config?
 #from pathlib import Path # Might use for file operations?
 
 class Morse:
-    def __init__(self, db:str = 'morse.yaml'):
-        '''Initialize Morse class.
-        Takes in a reference to a .yaml file or dictionary of single character keys.'''
+    '''Functional component of project.py.
+        txt_to_morse: given text, return morse code equivalent.
+        morse_to_txt: given morse, return text equivalent.
+        @class touch_config: creates config file if it doesn\'t exist.'''
+    def __init__(self, data:str = 'morse.yaml'):
         # Import YAML file with morse data
-        with open(db, mode='r') as file:
+        with open(data, mode='r', encoding='utf-8') as file:
             self.lookup = yaml.load(file, Loader=yaml.BaseLoader)
             if not all(len(ch) == 1 and isinstance(ch) == str for ch in self.lookup):
                 raise ValueError('All keys in .yaml file must be a char.')
@@ -20,26 +25,26 @@ class Morse:
     def txt_to_morse(self, txt: str) -> str:
         '''Given a string of text, use lookup array to generate morse code equivalent of text.'''
         to_return = ''
-        for c in txt:
-            if c in self.lookup:
-                to_return += self.lookup[c] 
+        for char in txt:
+            if char in self.lookup:
+                to_return += self.lookup[char]
 
         return to_return
 
     def morse_to_txt(self, morse: str) -> str:
         '''Given a string of morse, use lookup array to generate text equivalent.'''
-        ...
 
     @classmethod
-    def touch_config():
+    def touch_config(cls):
         '''Creates configuration file and prints its location on disk.
         May remove.'''
         # Create config file if it doesn't exist
-        ...
 
 
 def main():
+    '''Called on program start.'''
     morse = Morse()
+    morse.txt_to_morse("SOS")
 
 
 if __name__ == '__main__':
