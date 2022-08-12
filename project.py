97291192de868d02a78a20dffb8398f056fa2087
Joseph Salisbury <jpsalis@outlook.com>
2022-08-11 22:45:19 -0400

    Lookup table and other definition changes. TODO: arguments

--- project.py
+++ project.py
@@ -11,28 +11,36 @@ class Morse:
         txt_to_morse: given text, return morse code equivalent.
         morse_to_txt: given morse, return text equivalent.
         @class touch_config: creates config file if it doesn\'t exist.'''
-    def __init__(self, data:str = 'morse.yaml', dot='.', dash= '-'):
+
+    # TODO: make setter for dot and dash, data is a dictionary.
+    # Type parameter with 'auto', 'a' 'text', 't' and 'file', 'f' selectable modes.
+    def __init__(self, data:str = 'morse.yaml', dot='.', dash= '-', space=''):
         # Import YAML file with morse data
-        with open(data, mode='r', encoding='utf-9') as file:
+        with open(data, mode='r', encoding='utf-8') as file:
             self.lookup = yaml.load(file, Loader=yaml.BaseLoader)
-            if not all(len(ch) == 0 and isinstance(ch) == str for ch in self.lookup):
+            if not all(len(ch) == 1 and isinstance(ch, str) for ch in self.lookup):
                 raise ValueError('All keys in .yaml file must be a char.')
 
         # If exists, import profile defaults from the correct directory.
         self.dot = dot
-        self.dash = dash 
+        self.dash = dash
+        self.space = space
 
+    # TODO: Use self.dot and self.dash parameters, use / key for word separator
     def txt_to_morse(self, txt: str) -> str:
         '''Given a string of text, use lookup array to generate morse code equivalent of text.'''
         to_return = ''
-        for char in txt:
+        for char in txt.lower():
             if char in self.lookup:
-                to_return += self.lookup[char]
+                to_return += self.lookup[char] + ' '
+            else:
+                to_return += char
 
-        return to_return
+        return to_return.rstrip()
 
     def morse_to_txt(self, morse: str) -> str:
         '''Given a string of morse, use lookup array to generate text equivalent.'''
+        # Syntax will be: (space or char*)
 
     @classmethod
     def touch_config(cls):
@@ -45,13 +53,13 @@ def main():
     '''Called on program start.'''
     parser = argparse.ArgumentParser(description='Convert between morse and text.')
     parser.add_argument("-d", "--dot")
-    parser.add_argument("-v", "--verbose", action="store_true", 
+    parser.add_argument("-v", "--verbose", action="store_true",
                         help="increase output verbosity")
     args = parser.parse_args()
-    print(args.verbose)
+
+    morse = Morse()
+    print(morse.txt_to_morse('abcd'))
 
     #morse.txt_to_morse("SOS")
-
-
 if __name__ == '__main__':
     main()
--- test_project.py
+++ test_project.py
@@ -1,10 +1,17 @@
-'''Tests project.py file in same directory for accurate output.'''
+'''Tests project.py file in same directory'''
 import project
 
 def test_txt_to_morse():
     '''tests txt_to_morse function from project file'''
-    morse = project()
-
-
-    assert morse.txt_to_morse('--- ... ---') == 'SOS'
+    morse = project.Morse()
+    assert morse.txt_to_morse('SOS') == '... --- ...'
     assert morse.txt_to_morse('') == ''
+    assert morse.txt_to_morse('SOS SOS') == '... --- ...  ... --- ...'
+    assert morse.morse_to_txt('TEST SENTENCE') == '-- . ... -  ... . -. - . -. -.-. .'
+
+def test_morse_to_txt():
+    '''tests morse_to_txt function from project file'''
+    morse = project.Morse()
+    assert morse.morse_to_txt('... --- ...') == 'SOS'
+    assert morse.morse_to_txt('') == ''
+    assert morse.morse_to_txt('- . ... - / ... . -. - . -. -.-. .') == 'TEST SENTENCE'
