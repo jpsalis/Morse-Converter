8bb086a34687f6a5a8ea566fa97dc0534a90af81
Joseph Salisbury <jpsalis@pop-os.localdomain>
2022-08-17 03:47:46 -0400

    Changes to Morse class to be more extensible. command line changes.

--- README.md
+++ README.md
@@ -1,31 +1,38 @@
 # Morse-Converter
 Offline command line program designed to convert back-and-forth between text and morse code, with a real-time interpreter.
 
- * Allows a text or morse message to be converted into a `.wav` file, with configurable word speed and frequency.
  * Custom short tone and long tone mappings can be assigned to match the input type.
  * defaults for tones, audio frequency and word speed can be assigned in `~/.config/morseconv/defaults.yaml` if desired (Data type subject to change until submission).
+ * Compatability with bash shell's input and output operators, allowing files to be imported rather than text
+
 
 ## Usage
 `morseconv`
 
 `morseconv [-t | -m] [-s char] [-l char] text [text ...]`
 
-`morseconv [Input Hint] [-s char] [-l char] [-i]`
     
 ### Examples 
     morseconv "test text" : implied text > morse
     morseconv "... --- ..." : implied morse > text
-    morseconv                                               : realtime interpreter
-    morseconv -t "this is text" 
-    morseconv -m "... --- ..." 
-    morseconv text here with no parameter?                  : conversion without (Might not include)
+    morseconv -t "this is text"
+    morseconv -m "... --- ..."
+    morseconv text here with no parameter?
 
 ## Flags
-### Input Type Hint:
+Operations that will be supported by the complete project.
+
+### Basic
     -m, --morse         input is morse, expect alpha output
-    -t, --text         input is alpha, expect morse output
+    -t, --text          input is alpha, expect morse output
 
 ### Etc:
     -h, --help          help dialog and version
-    -v, --verbose       explain what is being done
-    -d, --debug         Information for error diagnosis
+    --version           effecive program version
+
+    -e, --errorhandle   change how invalid text is handled
+    -f, --file          use alternate source for lookup table. Must be yaml file.
+
+## Other options:
+    -s, --short         short tone override (Must be a single character or list of possible)
+    -l, --long          long tone override (Must be a single character or list of possible)
--- project.py
+++ project.py
@@ -5,36 +5,47 @@ the user is returned a string of the opposite type."""
 import argparse
 import yaml  # Might use for config?
 
+LOOKUP_DIR = 'morse.yaml'
+
+
 class Morse:
-    '''Functional component of project.py.
+    """Functional component of project.py.
     txt_to_morse: given text, return morse code equivalent.
     morse_to_txt: given morse, return text equivalent.
-    @class touch_config: creates config file if it doesn\'t exist.'''
+    """
 
-    # TODO: Import dict separate from .yaml 
-    def __init__(self, data: str = "morse.yaml"):
-        with open(data, mode="r", encoding="utf-8") as file:
-            self.lookup = yaml.load(file, Loader=yaml.BaseLoader)
-            if not all(len(ch) == 1 and isinstance(ch, str) for ch in self.lookup):
-                raise ValueError("All keys in .yaml file must be a char.")
+    def __init__(self, data: dict, err_mode: str = "p"):
+        self.lookup = data
+
+    @property
+    def lookup(self):
+        """Stores a dictionary of key-value pairs.
+        all keys must be a single character."""
+        return self._lookup
+
+    @lookup.setter
+    def lookup(self, l):
+        if not all(len(ch) == 1 and isinstance(ch, str) for ch in l):
+            raise ValueError("All keys must be a char.")
+        self._lookup = l
 
     def txt_to_morse(self, txt: str) -> str:
-        '''Given a string of text, use lookup array to generate morse code equivalent of text.'''
-        to_return = ''
+        """Given a string of text, use lookup array to generate morse code equivalent of text."""
+        to_return = ""
         for char in txt.upper():
             if char in self.lookup:
-                to_return += self.lookup[char] + ' '
+                to_return += self.lookup[char] + " "
             else:
                 to_return += char
 
-        return to_return.rstrip('/ ')
+        return to_return.rstrip("/ ")
 
     # TODO: Future refactor
     def morse_to_txt(self, morse: str) -> str:
-        '''Given a string of morse, use lookup array to generate text equivalent.'''
+        """Given a string of morse, use lookup array to generate text equivalent."""
         # May change this
         if len(morse) == 0:
-            return ''
+            return ""
         morse_chars = morse.split(" ")
         to_return = str()
         for char in morse_chars:
@@ -43,56 +54,58 @@ class Morse:
                     key for key, value in self.lookup.items() if value == char
                 )
             except StopIteration:
-                to_return += '�'
+                to_return += "�"
 
         return to_return
 
-    def auto_conv(self, string: str) -> str:
-        # Take in a string, determine the type if possible
-        # Assume 
-        ...
+    def auto_conv(self, inp: str) -> str:
+        """Take in a string, determine the type if possible"""
+        res = all(char in " /-." for char in inp)
+        return self.morse_to_txt(inp) if res else self.txt_to_morse(inp)
+
 
 def main():
     parser = make_parser()
     args = parser.parse_args()
-    words = ' '.join(args.words)
+    words = " ".join(args.words)
 
-    morse = Morse()
-    if args.text:
+    with open(LOOKUP_DIR, mode="r", encoding="utf-8") as file:
+        lookup = yaml.load(file, Loader=yaml.BaseLoader)
+
+    print (args.errmode)
+    morse = Morse(lookup)
+    if args.morse:
         print(morse.morse_to_txt(words))
-    elif args.morse:
+    elif args.text:
         print(morse.txt_to_morse(words))
     else:
         print(morse.auto_conv(words))
 
 
 def make_parser():
-    '''Called on program start.'''
+    """Called on program start."""
     # Convert multiple arguments into single string
     parser = argparse.ArgumentParser(
-        description='Convert between morse and text. Will automatically guess the input type, but can be overridden.'
+        description="Convert automatically between morse and text."
     )
-    parser.add_argument('--version', action='version', version='%(prog)s 0.1.0')
-    # Verbose
-    parser.add_argument(
-        '-v', '--verbose', action='store_true', help='increase output verbosity'
-    )
-    # Type Hint
+    parser.add_argument("--version", action="version", version="%(prog)s 0.1.0")
+    # Input Hint
     group = parser.add_mutually_exclusive_group()
-    group.add_argument('-t', '--text', action='store_true', help='source is text. Convert to morse')
-    group.add_argument('-m', '--morse', action='store_true', help='source is morse. Convert to text')
+    group.add_argument(
+        "-t", "--text", action="store_true", help="source is text. Convert to morse"
+    )
+    group.add_argument(
+        "-m", "--morse", action="store_true", help="source is morse. Convert to text"
+    )
     # String input
-    parser.add_argument('words', type=str, nargs='+', help='morse or txt source to convert')
+    parser.add_argument(
+        "words", type=str, nargs="+", help="morse or txt source to convert"
+    )
 
-    # TODO: What to do with errors? Pass, Print, Raise? Default: Print invalid char
+    parser.add_argument("-e", "--errmode", default="print", choices=["ignore","print", "raw" ,"err"])
 
     return parser
 
 
-def touch_config():
-    '''Creates configuration file and prints its location on disk.
-    May remove.'''
-    ...
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
